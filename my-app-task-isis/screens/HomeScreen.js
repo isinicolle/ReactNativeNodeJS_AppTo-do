3203fd556203987b4370990575b8659fe37f3078
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-08 15:49:19 -0600

    arreglitos
    arreglitos

--- my-app-task-isis/screens/HomeScreen.js
+++ my-app-task-isis/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-import TaskList from "../components/TaskList";
+import TaskList from "../components/TasksList";
 
 const HomeScreen = () => {
   return (
