469e87eab2288d63d8d9ad4bc385a0b043d485c5
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-08 14:06:15 -0600

    .
    .

--- my-app-task-isis/App.js
+++ my-app-task-isis/App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function App() {
+import HomeScreen from "./screens/HomeScreen";
+import TaskFormScreen from "./screens/TaskFormScreen";
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo soy Isis</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "Tasks App",
+            headerStyle: {
+              backgroundColor: "#222f3e",
+            },
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("TaskFormScreen")}
+              >
+                <Text style={{ color: "#fff", marginRight: 20, fontSize: 15 }}>
+                  New
+                </Text>
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="TaskFormScreen"
+          component={TaskFormScreen}
+          options={{
+            title: 'Create a Task',
+            headerStyle: {
+              backgroundColor: "#222f3e",
+            },
+            headerTintColor: "#fff",
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
