0b519eaf31429b19eb94be77e5fe2be3a81f0a93
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-08 19:33:16 -0600

    Update TasksList.js

--- my-app-task-isis/components/TasksList.js
+++ my-app-task-isis/components/TasksList.js
@@ -1,73 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, Alert, RefreshControl } from "react-native";
-import { useIsFocused } from "@react-navigation/native";
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import { deleteTask, getTasks } from "../api";
-import TaskItem from "./TaskItem";
-
-const TasksList = ({ navigation }) => {
-  const [tasks, setTasks] = useState([]);
-  const [refreshing, setRefreshing] = React.useState(false);
-  const isFocused = useIsFocused();
-
-  const getUsers = async () => {
-    try {
-      const tasks = await getTasks();
-      setTasks(tasks);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const onRefresh = React.useCallback(async () => {
-    setRefreshing(true);
-    // wait(2000).then(() => setRefreshing(false));
-    await getUsers();
-    setRefreshing(false);
-  }, []);
-
-  const handleDelete = (id) => {
-    Alert.alert("Delete Task", "Are you sure you want to delete the task", [
-      {
-        text: "Cancel",
-        style: "cancel",
-      },
-      {
-        text: "Ok",
-        onPress: async () => {
-          await deleteTask(id);
-          await getUsers();
-        },
-      },
-    ]);
-  };
-
-  useEffect(() => {
-    getUsers();
-    console.log("called");
-  }, [isFocused]);
-
-  const renderItem = ({ item }) => (
-    <TaskItem task={item} handleDelete={handleDelete} />
-  );
+const TaskItem = ({ task, handleDelete }) => {
+  const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={{ flex: 1, width: "90%" }}>
-      <FlatList
-        data={tasks}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            colors={["#78e08f"]}
-            progressBackgroundColor="#0a3d62"
-          />
-        }
-      />
-    </SafeAreaView>
+    <View style={styles.itemContainer}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("TaskFormScreen", { id: task.id })}
+      >
+        <Text style={styles.itemTitle}>{task.title}</Text>
+        <Text style={{ color: "#8395a7" }}>{task.description}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ backgroundColor: "#ee5253", padding: 7, borderRadius: 5 }}
+        onPress={() => handleDelete(task.id)}
+      >
+        <Text style={{ color: "#fff" }}>Delete</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-export default TasksList;
+const styles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: "#333333",
+    padding: 20,
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  itemTitle: {
+    color: "#ffffff",
+  },
+});
+export default TaskItem;
